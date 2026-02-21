9ab82709ed21a7c146ee4079fd33a0bab1fc92cc
Houzibe <dhouzibe1@gmail.com>
2026-02-21 16:59:48 +0100

    created a reusable currency conversion function

--- converter.py
+++ converter.py
@@ -13,10 +13,13 @@ exchange_rates = {
     "GBP" : 0.00055154  
     }
 
-print()
-enteredAmount = input("Enter amount(in Naira): ")
+def currency_conversion(amount, rate):
+    return round(amount*rate,2)
 
+print()
+enteredAmount = float(input("Enter amount(in Naira): "))
 print("Conversion Outcome:")
+print(f"{enteredAmount} Naira is {currency_conversion(enteredAmount,exchange_rates["USD"])} USD;")
 
 
 print()
