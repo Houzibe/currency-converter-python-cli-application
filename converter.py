0f7dbd6613d18cc86cd43c57fc8faa77816b6770
Houzibe <dhouzibe1@gmail.com>
2026-02-21 16:44:54 +0100

    capture user entered amount in Naira

--- converter.py
+++ converter.py
@@ -1,16 +1,23 @@
+#-----------------------------------------------------------
 # Currency Converter - Python Cli App
+#-----------------------------------------------------------
+print()
+print("****************************************************")
+print("      Welcome to Currency Converter Application  ")
+print("****************************************************")
+print("Note: This App converts naira to USD, EUR, GBP.")
 
 exchange_rates = {
     "USD" : 0.00074430, 
     "EUR" : 0.00063130, 
     "GBP" : 0.00055154  
     }
+
 print()
-print("****************************************************")
-print("      Welcome to Currency Converter Application  ")
-print("****************************************************")
-print("Note: This App convert naira to USD, EUR, GBP.")
-print()
+enteredAmount = input("Enter amount(in Naira): ")
+
+print("Conversion Outcome:")
+
 
 print()
 print("****************************************************")
