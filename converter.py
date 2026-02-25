ccc3e65189e124e433dc4777053c79bf10dc720d
Houzibe <dhouzibe1@gmail.com>
2026-02-25 17:32:07 +0100

    add two additional currency XAF and GHS, also the results in ReadMe

--- converter.py
+++ converter.py
@@ -5,12 +5,14 @@ print()
 print("****************************************************")
 print("      Currency Converter CLI Application  ")
 print("****************************************************")
-print("Note: This App converts naira to USD, EUR, GBP.")
+print("Note: This App converts naira to USD, EUR, GBP, XAF, GHS")
 
 exchange_rates = {
-    "USD" : 0.00074430, 
-    "EUR" : 0.00063130, 
-    "GBP" : 0.00055154  
+    "USD" : 0.00073862, 
+    "EUR" : 0.00062565, 
+    "GBP" : 0.00054549,
+    "XAF" : 0.41040250,
+    "GHS" : 0.00786432  
     }
 
 def currency_conversion(amount, rate):
@@ -25,6 +27,8 @@ print("----------------------------------------------------")
 print(f"{enteredAmount} Naira is {currency_conversion(enteredAmount,exchange_rates["USD"])} USD;")
 print(f"{enteredAmount} Naira is {currency_conversion(enteredAmount,exchange_rates["EUR"])} EUR;")
 print(f"{enteredAmount} Naira is {currency_conversion(enteredAmount,exchange_rates["GBP"])} GBP;")
+print(f"{enteredAmount} Naira is {currency_conversion(enteredAmount,exchange_rates["XAF"])} XAF;")
+print(f"{enteredAmount} Naira is {currency_conversion(enteredAmount,exchange_rates["GHS"])} GHS;")
 print("----------------------------------------------------")
 print("Thank you for using our currency converter today!")
 print()
